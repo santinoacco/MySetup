b47a1bd024feb92af67485da27245adcc3510362
Santi Noacco <santinoacco@gmail.com>
2020-02-20 16:33:36 -0300

    first commit, created some folders

---
+++ Python/NoaccoLibrary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# here I will store my useful functions
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import ROOT as r
+import SwanImports
---
+++ README.md
@@ -0,0 +1,4 @@
+**Here I will store my particular configuration for the many programs that**
+**enhance my workflow**
+
+
