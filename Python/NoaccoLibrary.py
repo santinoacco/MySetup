439d8dbf899cf49df0424c6df489beb2035d96cd
Santi Noacco <santinoacco@gmail.com>
2020-04-05 18:10:21 -0300

    added some plot and other functions

--- Python/NoaccoLibrary.py
+++ Python/NoaccoLibrary.py
@@ -2,15 +2,14 @@
 
 # here I will store my useful functions
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import ROOT as r
-import SwanImports
+#import ROOT as r
+#import SwanImports
 
 
-
-def Config_Parse():
+import argparse
+def config_Parse():
     """
     Set all the configuration to your parser object.
 
@@ -25,5 +24,66 @@ def Config_Parse():
     parser.add_argument('-M','--MaxEvents', required=False, help='Set maximum of events. Default -1 == all', type=int, default=-1)
     return parser
 
+# == create dir
+from os import mkdir
+from os.path import join
+def build_dir(Dir_name,File_name,Extension):
+    assert type(Dir_name) == str, 'Dir_name must be a string'
+    assert type(Extension) == str, 'Extension must be a string'
+    try: mkdir(Dir_name)
+    except: pass
+    outfile = join(Dir_name,File_name+Extension)
+    return outfile
+
+# == Plots
+import matplotlib.pyplot as plt
+import seaborn as sns
+def my_seaborn_style():
+    pass
+
+def my_plt_style(plot_obj):
+    pass
+    #assert(type(plot_obj) == plt.figure.Figure), 'check plot_obj type'
+    #plot_obj.set_xlabel 
+    #return
+
+#def my_plotter_2D(plot_name, x_data, y_data, param_dict):
+def my_plotter_2D(ax, x_data, y_data, param_dict):
+    """
+        A helper function to make a graph
+
+        Parameters
+        ----------
+        ax : Axes
+            The axes to draw to
+
+        data1 : array
+           The x data
+
+        data2 : array
+           The y data
+
+        param_dict : dict
+           Dictionary of kwargs to pass to ax.plot
+
+        Returns
+        -------
+        out : list
+            list of artists added
+    """
+    #common_plots = {
+    #        'plot':ax.plot, 'scatter':ax.scatter,
+    #        'errorbar':ax.errorbar, 'date':ax.plot_date,
+    #        'hist':ax.hist
+    #        }
+
+    #out = common_plots[plot_name](x_data, y_data, **param_dict)
+    out = ax.plot(x_data, y_data, **param_dict)
+    #ax = out.gca()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.legend()
+
+    return out
 # ===== ROOT ===== #
 
